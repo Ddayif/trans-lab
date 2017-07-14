d69353764321fd9bec5036180c5f0912ca84d1df
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-13 22:49:08 -0300

    Agregando input y boton a index-5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /* Holiii acá va tu código también */
 $( document ).ready(function(){
-	$(".button-collapse").sideNav();
+	$(".button-collapse").sideNav();// muestra el side-nav en cada uno de los index, menos en index.html (Home)
+	$('select').material_select(); // inicializa el selector de numero de tarjeta que se encuentra en el index ver-saldo.html
 })
