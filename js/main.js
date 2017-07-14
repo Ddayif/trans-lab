251d5b26615f25e21cc3fff9ae45dbea0c3abd06
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-14 12:03:51 -0300

    Agregando validaciones a mis campos correo y contraseña

--- js/main.js
+++ js/main.js
@@ -22,6 +22,30 @@ $( document ).ready(function(){
 		$("#number_card_calcular").val("");
 	});
 
+	$
+
 })
 
 
+$("#iniciar-sesion").click(function(){
+	var pass = $("#pass").val();
+    var mail = $("#email").val();
+    var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);//<--- con esto vamos a validar el formato del correo
+
+
+    if(mail ==""){// valida si mi campo email esta vacio, si es asi, se mostrara un alert con la informacion
+        alert("El campo Email es obligatorio");
+    }
+    if(pass ==""){//valida si mi campo password esta vacio, si es asi, se mostrara un alert con la informacion
+		alert("El campo contraseña es obligatorio")
+	}
+    else if(!caract.test(mail)){// si mi campo email no cumple con el formato, este mostrar un alert con la informacion
+        alert("Debes ingresar un formato válido");
+    }
+    else if(pass.length > 8){// si mi password es mayor a 8 digitos, este mostrara un alert indicando que solo se pueden igresar como maximo 8 caracteres
+		alert("Tu contraseña solo puede contener máximo 8 carácteres")
+	}
+    else{
+        $("#iniciar-sesion").attr("href", "opciones.html");// si todas las condiciones no se cumplen, al presionar el boton con el ID iniciar sesion, me redireccionara a mi nuevo index (opciones)
+    }
+  });
